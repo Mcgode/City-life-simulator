6632625ee4ce8c82f23f54c91cb91786e197fe67
max <max@godefroy.net>
2017-02-11 22:06:22 +0100

    More code comments

--- Assets/Scripts/World/Coords2D.cs
+++ Assets/Scripts/World/Coords2D.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public struct Coords2D {
 
+
 	public int x, y;
+
 
 	public Coords2D(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	public override string ToString ()
-	{
-		return "(" + x + ";" + y + ")";
-	}
 
+	public override string ToString ()
+	{ return "(" + x + ";" + y + ")";}
+
+
+	// Operator overload for the struct (+, -, *)
 	public static Coords2D operator +(Coords2D c1, Coords2D c2) {
 		return new Coords2D (c1.x + c2.x, c1.y + c2.y);
 	}
@@ -23,16 +26,25 @@ public struct Coords2D {
 	public static Coords2D operator -(Coords2D c1, Coords2D c2) {
 		return new Coords2D (c1.x - c2.x, c1.y - c2.y);
 	}
-
+		
+	// This is a scalar product, rather than just a product
 	public static int operator *(Coords2D c1, Coords2D c2) {
 		return c1.x * c2.x +  c1.y * c2.y;
 	}
 
+
+	// Returns the 2D coords of the given game object. 
 	public static Coords2D getCoords(GameObject obj) {
 		Vector3 pos = obj.transform.position;
 		return new Coords2D (Mathf.RoundToInt (pos.x), Mathf.RoundToInt (pos.y));
 	}
 
+
+	/* Returns the two other points to make a square : 
+			- - T			X - T
+			- - -	 --\	- - - 		O is the origin (this), T is the target (other)
+			- - -	 --/  	- - -   	Returns X and Y coordinates.
+			O - -			O - Y 														*/
 	public List<Coords2D> squareTwoPoints(Coords2D other) {
 		Coords2D diff = other - this;
 		return new List<Coords2D> () {
